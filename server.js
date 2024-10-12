6b6e89aee358c7c799bcf018face59e7962439bb
sanket622 <sanketkumar0068@gmail.com>
2024-10-12 17:07:07 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,44 @@
+import express from 'express'
+import { createServer } from 'http'
+import { v4 as uuidv4 } from 'uuid';
+import { Server } from 'socket.io';
+import { ExpressPeerServer } from 'peer';
+
+const app = express()
+app.use(express.static('public'))
+const server = createServer(app)
+const peerServer = ExpressPeerServer(server,{
+    debug:true
+})
+
+
+app.set('view engine','ejs')
+app.use('/peerjs',peerServer)
+
+app.get('/',(req,res)=>{
+    res.redirect(`/${uuidv4()}`)
+})
+
+app.get('/:room',(req,res)=>{
+    res.render('room',{roomId:req.params.room})
+})
+
+const io = new Server(server, {
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST']
+    }
+});
+
+io.on('connection',socket =>{
+    socket.on('join-room',(roomId, userId )=>{
+       socket.join(roomId)
+       socket.to(roomId).emit('user-connected', userId)
+       socket.on('message',message =>{
+        io.to(roomId).emit('createMessage',message)
+       })
+    })
+})
+server.listen(3000,()=>{
+    console.log('server is running on port 3000');
+})
